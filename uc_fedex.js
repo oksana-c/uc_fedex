37a71e8f48cd268b1d8faa7cf5db397b65f8b97a
Tim Rohaly <tr@202830.no-reply.drupal.org>
2009-12-15 23:48:19 +0000

    Start breaking out specialty functionality into separate include files

---
+++ uc_fedex.js
@@ -0,0 +1,10 @@
+// $Id$
+
+if (Drupal.jsEnabled) {
+  (function ($) {  // Put definition of $ into local scope so jQuery code
+                   // doesn't conflict with other JavaScript libraries
+  $(document).ready(function() {
+  });
+
+  }) (jQuery)  // End local scope for $
+}
