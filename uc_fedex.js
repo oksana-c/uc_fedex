b6096ddfe73b4ede3fab1065cc218a1629386642
Tim Rohaly <tr@202830.no-reply.drupal.org>
2010-01-11 05:52:36 +0000

    Switch to Drupal.behaviors

--- uc_fedex.js
+++ uc_fedex.js
@@ -1,10 +1,8 @@
 // $Id$
 
-if (Drupal.jsEnabled) {
-  (function ($) {  // Put definition of $ into local scope so jQuery code
-                   // doesn't conflict with other JavaScript libraries
-  $(document).ready(function() {
-  });
+(function ($) {  // Put definition of $ into local scope so jQuery code
+                 // doesn't conflict with other JavaScript libraries
+  Drupal.behaviors.uc_fedex = function() {
+  };
 
-  }) (jQuery)  // End local scope for $
-}
+}) (jQuery)  // End local scope for $
